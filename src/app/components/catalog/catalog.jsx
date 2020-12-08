d6da7d50661d9a137fc5ff48dcca41e47acf7130
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-08 17:09:21 +0100

    Moved images list to separate component

--- src/app/components/catalog/catalog.jsx
+++ src/app/components/catalog/catalog.jsx
@@ -1,50 +1,18 @@
-import {
-  Container,
-  Grid,
-  List,
-  ListItem,
-  Paper,
-  Typography,
-} from '@material-ui/core';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import {
-  selectCatalog,
-  requestCatalogUpdate,
-  requestImageTagsList,
-} from './../../redux/slices/catalogSlice';
-import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid } from '@material-ui/core';
+import React from 'react';
 
-import strings from '../../localization/strings';
-import { useDispatch } from 'react-redux';
+import ImagesList from './imagesList';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: '2em',
   },
-  title: { padding: '0.4em' },
-  listPaper: {
-    width: '100%',
-    maxWidth: '30em',
-    backgroundColor: theme.palette.background.paper,
-  },
-  listElement: {},
 }));
 
 const Catalog = (props) => {
-  const dispatch = useDispatch();
   const classes = useStyles();
-  const images = useSelector(selectCatalog);
-
-  useEffect(() => {
-    if (!images || images.length === 0) {
-      dispatch(requestCatalogUpdate());
-    }
-  }, []);
-
-  const retrieveTagsList = (imageName) => () => {
-    dispatch(requestImageTagsList(imageName));
-  };
 
   return (
     <Container className={classes.root}>
@@ -56,25 +24,7 @@ const Catalog = (props) => {
         alignItems='center'
       >
         <Grid item container>
-          <Paper className={classes.listPaper} elevation={6}>
-            <Typography variant='h4' className={classes.title}>
-              {strings.CATALOG.TITLE}
-            </Typography>
-            <List>
-              {images.map((element) => {
-                return (
-                  <ListItem
-                    button
-                    className={classes.listElement}
-                    key={element}
-                    onClick={retrieveTagsList(element)}
-                  >
-                    {element}
-                  </ListItem>
-                );
-              })}
-            </List>
-          </Paper>
+          <ImagesList />
         </Grid>
         <Grid item container></Grid>
       </Grid>
---
+++ src/app/components/catalog/imagesList.jsx
@@ -0,0 +1,72 @@
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  selectCatalog,
+  requestCatalogUpdate,
+  requestImageTagsList,
+} from './../../redux/slices/catalogSlice';
+import { makeStyles } from '@material-ui/core/styles';
+
+import strings from '../../localization/strings';
+import { useDispatch } from 'react-redux';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2em',
+  },
+  title: { padding: '0.4em' },
+  listPaper: {
+    width: '100%',
+    maxWidth: '30em',
+    backgroundColor: theme.palette.background.paper,
+  },
+  listElement: {},
+}));
+
+const ImagesList = (props) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const images = useSelector(selectCatalog);
+
+  useEffect(() => {
+    if (!images || images.length === 0) {
+      dispatch(requestCatalogUpdate());
+    }
+  }, []);
+
+  const retrieveTagsList = (imageName) => () => {
+    dispatch(requestImageTagsList(imageName));
+  };
+
+  return (
+    <Paper className={classes.listPaper} elevation={6}>
+      <Typography variant='h4' className={classes.title}>
+        {strings.CATALOG.TITLE}
+      </Typography>
+      <List>
+        {images.map((element) => {
+          return (
+            <ListItem
+              button
+              className={classes.listElement}
+              key={element}
+              onClick={retrieveTagsList(element)}
+            >
+              {element}
+            </ListItem>
+          );
+        })}
+      </List>
+    </Paper>
+  );
+};
+
+export default ImagesList;
