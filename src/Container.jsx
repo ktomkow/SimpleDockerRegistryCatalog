7743952476178c59c7994cb80746c685d1bcec91
ktomkow <krzysztof.tomkow@gmail.com>
2021-03-04 22:00:23 +0100

    Situation from work

--- src/Container.jsx
+++ src/Container.jsx
@@ -3,20 +3,28 @@ import Element from './Element';
 
 const Container = () => {
   const [elements, setElements] = useState([<Element name='Jacek' />]);
+  const [isVisible, setVisible] = useState(true);
 
-const handleClick = () => {
- const element = Object.assign([], elements);
- element.push(<Element name="Placek"/>);
- setElements(element);
-};
+  const handleClick = () => {
+    const element = Object.assign([], elements);
+    element.push(<Element name='Placek' />);
+    setElements(element);
+  };
+
+  const swap = () => {
+    setVisible(!isVisible);
+  };
 
   return (
     <div>
       <h1>Hera are elements</h1>
       <button onClick={handleClick}>push</button>
-      {elements.map((e, i) => (
-        <div key={i}>{e}</div>
-      ))}
+      <button onClick={swap}>swap</button>
+
+      <div className='actual-container'>
+        <p>something</p>
+        {isVisible && elements.map((e, i) => <div key={i}>{e}</div>)}
+      </div>
     </div>
   );
 };
